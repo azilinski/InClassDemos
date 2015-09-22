344a5d4cb217e51f8b6581c354da8bcaac5b3611
azilinski <alexanderzilinski@gmail.com>
2015-09-22 16:21:50 -0600

    setup of DLL Admin

---
+++ eRestaurantDemo/eResturauntSystem/BLL/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using eResturauntSystem.Entities;
+using eResturauntSystem.DAL;
+using System.ComponentModel; // use for ODS access
+
+#endregion
+
+namespace eResturauntSystem.BLL
+{
+    [DataObject]
+    public class AdminController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<SpecialEvent> SpecialEvent_List()
+        {
+            using (var context = new eRestaurantContext())
+            {
+                //Retrive the data from the special events table on sql
+                //to do so we will use the DbSet in eResturantContext called SpecialEvents
+
+                //method syntax
+                return context.SpecialEvents.OrderBy(x => x.Description).ToList();
+
+            }
+        }
+    }
+}
