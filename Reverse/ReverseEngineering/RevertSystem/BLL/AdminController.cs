835b2000cf40b9ba087b3ff645409184a6880134
azilinski <alexanderzilinski@gmail.com>
2015-11-12 16:34:59 -0700

    Reverse Engineering

---
+++ Reverse/ReverseEngineering/RevertSystem/BLL/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using RevertSystem.DAL;
+using RevertSystem.Entities;
+using System.ComponentModel;
+#endregion
+
+namespace RevertSystem.BLL
+{
+    public class AdminController
+    {
+
+        [DataObjectMethod(DataObjectMethodType.Select,false)]
+        public List<Employee> EntityList()
+        {
+            using(RevertContext context = new RevertContext())
+            {
+                return context.Employees.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Employee> QueryNav_List()
+        {
+            using (RevertContext context = new RevertContext())
+            {
+                var results = from item in context.EmployeeSkills
+                              where item.SkillID == 8
+                              select item.Employee;
+                return results.ToList();
+            }
+        }
+
+    }
+}
---
+++ Reverse/ReverseEngineeringWebsite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ReverseEngineeringWebsite.Startup))]
+namespace ReverseEngineeringWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
